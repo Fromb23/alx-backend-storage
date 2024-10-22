44d44b998d4589b5740b57eff06be1d120085088
Francis Rombo <fromb@kali.>
2024-10-22 19:24:07 +0000

    nosql

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -8,8 +8,4 @@ def list_all(mongo_collection):
     """
     return the list in collection
     """
-    mongo_list = []
-    if mongo_collection is None:
-        return mongo_list
-    else:
-        return mongo_list = list(mongo_collection.find())
+    return list(mongo_collection.find())
