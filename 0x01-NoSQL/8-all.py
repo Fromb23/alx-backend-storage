b8109b95435e6776f5923ed585064e69335c22ad
Francis Rombo <fromb@kali.>
2024-10-22 19:13:45 +0000

    nosql

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""
+    list all docs in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    return the list in collection
+    """
+    mongo_list = []
+    if mongo_collection is None:
+        return mongo_list
+    else:
+        return mongo_list = list(mongo_collection.find())
